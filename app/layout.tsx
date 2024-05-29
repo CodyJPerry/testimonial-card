fd0b4e7742022b2ca0f078a357658f1efe67f720
Cody Perry <perrysdevelopment@gmail.com>
2024-05-28 21:45:08 -0400

    Add content and background gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gradient-to-r from-[#F9FAFB] to-[#D2D6DB]`} >{children}</body>
     </html>
   );
 }
